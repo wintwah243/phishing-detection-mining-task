d89ba158c1ec12f04fff449d90838d30166c65dd
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-09-09 11:13:09 +0630

    update

--- phishing-detection/src/components/Hero.jsx
+++ phishing-detection/src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
     setResult(null);
 
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://phishing-detection-backend-a1yi.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
